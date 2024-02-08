463c0ed4104d7992fb0a8aa9a3229e35367af8d9
DM0k <129907524+DM0k@users.noreply.github.com>
2024-02-07 22:18:56 -0300

    Inicial

---
+++ ProvaVSComMDB/Model/Animais.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProvaVSComMDB.Model
+{
+    public class Animais
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nome { get; set;}
+        [MaxLength(100)]
+        public string? NomeCientifico { get; set;}
+        [MaxLength(100)]
+        public string? HabitatNatural { get; set;} 
+
+    }
+}
---
+++ ProvaVSComMDB/Model/_DbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProvaVSComMDB.Model
+{
+    public class _DbContext : DbContext
+
+    {
+
+        public DbSet<Animais> Animais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+
+        {
+
+            IConfigurationRoot Configuration = new ConfigurationBuilder()
+
+               .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+
+               .AddJsonFile("appsettings.json")
+
+            .Build();
+
+            var ConnectionString = Configuration.GetConnectionString("MariaDB");
+
+            OptionsBuilder.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+
+        }
+    }
+}
